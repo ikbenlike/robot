82913bd973d660e047c275c70952b3b1a6bac36d
Zeroji <zzeroji@gmail.com>
2018-12-09 14:50:09 +0100

    make ti c

--- src/genxml.cpp
+++ src/genxml.cpp
@@ -1,11 +1,22 @@
-#include <iostream>
-#include <fstream>
 
-using namespace std;
+#define TOWORD(_) # _
+#define PRINT(_) ;printf(_)
+#define PRINTS(_) PRINT(TOWORD(_))
+#define COORD(_) "<coord which=\"" TOWORD(_) "\">"
+#define COORD_ "</coord>"
+#define COORD2(_,__) COORD(_) "%s" COORD_ "\n",__
+
+    #include <stdio.h>
+    #include <stdlib.h>
 
 int main(int argc, char **argv){
-    ofstream xmlfile;
-    xmlfile.open("./data/data.xml");
+
+    PRINTS(<data>\n)
+    PRINT(COORD2(X, argv[1]))
+    PRINT(COORD2(Y, argv[2]))
+    PRINTS(<direction>) PRINT(argv[3]) PRINTS(</direction>)
+    PRINTS(</data>)
+
     xmlfile << "<data>" << endl;
     xmlfile << "\t<coord which=\"X\">";
     xmlfile << argv[1];
