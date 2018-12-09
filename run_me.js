ce3dfb839f3576672357cb23a08cd0046e76bc1a
ikbenlike <thewatcheriswatchinu@gmail.com>
2018-12-09 04:57:26 +0100

    run_me.js now translates direction letter to direction number for use in database

--- run_me.js
+++ run_me.js
@@ -25,5 +25,7 @@ for (i = 0; i<r.length; i++)
         newr = newr + r[i];
   else newr = newr + ")";
 
+const direction_facing_to = require('child_process').exec("bash ./scripts/translate.bash " + RobotDirectionFace + " && echo $?")
+
 FS = require('fs')
 require('fs').writeFileSync('file.sql', String(newr))
