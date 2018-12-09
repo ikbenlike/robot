d3d2f6773891944cc84d095835b7c7a23820560e
Zeroji <zzeroji@gmail.com>
2018-12-09 14:51:01 +0100

    fix bug remove code

--- src/genxml.c
+++ src/genxml.c
@@ -17,17 +17,18 @@ int main(int argc, char **argv){
     PRINTS(<direction>) PRINT(argv[3]) PRINTS(</direction>)
     PRINTS(</data>)
 
-    xmlfile << "<data>" << endl;
-    xmlfile << "\t<coord which=\"X\">";
-    xmlfile << argv[1];
-    xmlfile << "</coord>" << endl;
-    xmlfile << "\t<coord which=\"Y\">";
-    xmlfile << argv[2];
-    xmlfile << "</coord>" << endl;
-    xmlfile << "\t<direction>";
-    xmlfile << argv[3];
-    xmlfile << "</direction>" << endl;
-    xmlfile << "</data>" << endl;
-    xmlfile.close();
+    // xmlfile << "<data>" << endl;
+    // xmlfile << "\t<coord which=\"X\">";
+    // xmlfile << argv[1];
+    // xmlfile << "</coord>" << endl;
+    // xmlfile << "\t<coord which=\"Y\">";
+    // xmlfile << argv[2];
+    // xmlfile << "</coord>" << endl;
+    // xmlfile << "\t<direction>";
+    // xmlfile << argv[3]
+    ;
+    // xmlfile << "</direction>" << endl;
+    // xmlfile << "</data>" << endl;
+    // xmlfile.close();
     return 1;
 }
