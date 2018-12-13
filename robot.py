592904d7adfa96b766831e1247d3855a3c4a4cbf
Zeroji <zzeroji@gmail.com>
2018-12-13 12:40:13 +0100

    added support for smaller pythons

--- robot.py
+++ robot.py
@@ -25,9 +25,15 @@ from os import *
 def translate(str):
     for l in str:
         a = Switcharoo()
-        b = system(f"./bin/translate {l}")
+        try:#use the new f-strings because it's cool
+        	b = system(eval('f"./bin/translate {l}"'))
+        except Exception:#if don't work use the old
+        	b = system(Const.S.join(['./bin/translate', l]))
         c = a.dispatcher(b)
-        system(f"./scripts/movement.sh {c}")
+        try:#same thing here see above
+        	system(eval('f"./scripts/movement.sh {c}"'))
+        except Exception:
+        	system("./scripts/movement.sh" + Const.S + c)
     with open("data/data.xml", "r") as f:
         count = 0
         for l in f:
@@ -38,6 +44,6 @@ def translate(str):
                 print(l.split('>')[1].split('<')[0], end=Const.S)
             elif count==3:
                 print(Const.MAGICS[l.split('>')[1][:2]], end='')
-from builtins import *
+from builtins import * # fix open()
 with open("data/command.text", "r") as f:
     translate(f.read())
